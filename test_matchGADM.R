6f27c26d7aaf5eae6643b4dbaa4ff5f9a8cda5aa
katy <k.gaythorpe@imperial.ac.uk>
2019-07-08 11:57:44 +0100

    adding test and data

--- test_matchGADM.R
+++ test_matchGADM.R
@@ -8,8 +8,8 @@ old_version = "2"
 new_version = "36"
 
 ### IMPORT ###  ---------------------------------------------------------------------------------------------
-shp1_old = readShapePoly(paste0("../gadm", old_version, "/Africa_adm1.shp"))
-shp1_new = readOGR(paste0("../gadm", new_version, "/Africa_adm1.shp"),
+shp1_old = readShapePoly(paste0("data/gadm", old_version, "/Africa_adm1.shp"))
+shp1_new = readOGR(paste0("data/gadm", new_version, "/Africa_adm1.shp"),
                    use_iconv = TRUE, encoding = "UTF-8")
 
 names(shp1_new)[1] = "ISO"
@@ -37,4 +37,4 @@ BFA_comb_df = match_gadm(BFA_shp1_new, BFA_shp1_old, match_nearest = TRUE)
 # more accurate ish
 BFA_proportions = grid_match_multi(shp1_new = BFA_shp1_new, shp1_old = BFA_shp1_old, increment = 0.1)$proportions
 
-out = grid_match_multi(shp1_new = shp1_new, shp1_old = shp1_old, increment = 0.1)
+#out = grid_match_multi(shp1_new = shp1_new, shp1_old = shp1_old, increment = 0.1)
